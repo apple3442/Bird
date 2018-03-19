5f0348f07b2ab1c774abcd72be74278814edb848
Yoonsuk Dong <apple3442@hanmail.net>
2018-03-19 10:23:44 +0900

    Add P180319

---
+++ P180319/Assets/Script/Bird.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour, IGameobject
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void GameUpdate()
+    {
+
+    }
+}
---
+++ P180319/Assets/Script/Pipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour ,IGameobject{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void GameUpdate () {
+		
+	}
+}
